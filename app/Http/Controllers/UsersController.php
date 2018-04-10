6acc15bf62fb1303ef3937b327efa705b7a19a51
zhucong <tniub.cc@gmail.com>
2018-04-10 11:08:13 +0800

    User can upload

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -25,8 +26,16 @@ class UsersController extends Controller
 	/*
 	 *个人资料更新
 	 */
-	public function update(UserRequest $request,User $user){
-		$user->update($request->all());
+	public function update(UserRequest $request,User $user,ImageUploadHandler $uploader){
+		$data = $request->all();
+		//如果上传了头像
+		if($request->avatar){
+			$result = $uploader->save($request->avatar,'avatar',$user->id,362);
+			if($result){
+				$data['avatar'] = $result['path'];
+			}
+		}
+		$user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
 	}
 	
