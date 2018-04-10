c0dc5ae3372269aefacff392b1a9f3751f3376aa
zhucong <tniub.cc@gmail.com>
2018-04-10 19:40:58 +0800

    add topics

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //资源路由
 Route::resource('users','UsersController',['only' => ['show','update','edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
