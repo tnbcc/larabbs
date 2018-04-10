ff8e83fe87215dec8e29be78c96f0f03509f1ad5
zhucong <tniub.cc@gmail.com>
2018-04-10 14:54:14 +0800

    create categories

---
+++ database/migrations/2018_04_10_144527_seed_categories_data.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $data = [
+		   [
+			  'name' => '分享',
+              'description' => '分享发现，分享创造',			  
+			],
+			[
+			   'name' => '教程',
+			   'description' => '开发技巧，推荐拓展包',
+			],
+			[
+			   'name' => '问答',
+			   'description' => '请保持友善，互帮互助',
+			],
+			[
+			   'name' => '公告',
+			   'description' => '站点公告',
+			],
+		];
+		DB::table('categories')->insert($data);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
