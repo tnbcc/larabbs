611ff76db2d531f4904b60ef57c25eb5cf0e489e
zhucong <tniub.cc@gmail.com>
2018-04-13 14:33:21 +0800

    Topic detail page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -42,7 +42,7 @@ class TopicsController extends Controller
 		$topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('success', '发帖成功!');
+		return redirect()->route('topics.show', $topic->id)->with('success', '成功创建新话题!');
 	}
 
 	public function edit(Topic $topic)
@@ -56,7 +56,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新话题成功！');
 	}
 
 	public function destroy(Topic $topic)
@@ -64,7 +64,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('message', '删除话题成功！');
 	}
 	
 	 public function uploadImage(Request $request)
