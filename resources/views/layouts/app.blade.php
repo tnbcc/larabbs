71f1127917f1402e00762c2d961d65cc377f32a4
zhucong <tniub.cc@gmail.com>
2018-04-16 14:41:05 +0800

    Admin permissions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,9 +28,9 @@
 
         @include('layouts._footer')
     </div>
-    @if (app()->isLocal())
+    {{-- @if (app()->isLocal())
         @include('sudosu::user-selector')
-    @endif
+    @endif --}}
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 	@yield('scripts')
